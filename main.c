3c0cfa024fd53c35bd6a81f53e032be2651011e4
minhhieuhd98 <nguyenminhhieuhd98@gmail.com>
2018-03-27 00:04:35 +0700

    Push file main. function search_word was writted

--- main.c
+++ main.c
@@ -2,7 +2,52 @@
 #include <stdlib.h>
 #include <gtk/gtk.h>
 
+#include "libfdr/jrb.h"
+#include "libbtr/inc/btree.h"
+
+#define MAX_LEN 5000
+
+BTA* dic_bt;
+
+char* Search_Word(char *en_word , GtkWidget *search_entry , GtkWidget *text_view){
+	char* data_str = (char*)malloc(sizeof(char)*MAX_LEN);
+	int rsize;
+	btsel(dic_bt , en_word , data_str , sizeof(char)*MAX_LEN , &rsize) ;
+
+	return data_str;
+}
+
+int Translate_en_vi(GtkWidget *search_entry , GtkWidget *text_view){
+	gtk_text_view_set_justification (text_view , GTK_JUSTIFY_LEFT ) ;
+	gtk_text_view_set_left_margin (text_view , 8) ;
+
+	char *en_word , *vi_word ;
+	en_word = gtk_entry_get_text(search_entry) ;
+	vi_word = Search_Word(en_word , search_entry , text_view) ;
+
+	if(vi_word == NULL)
+		return 0 ;
+
+	GtkTextBuffer *buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(text_view));
+    GtkTextIter end , start;
+    gtk_text_buffer_get_end_iter(buffer, &end);      
+    gtk_text_buffer_get_start_iter(buffer, &start);
+    gtk_text_buffer_delete(buffer , &start ,&end) ;   
+    gtk_text_buffer_insert (buffer, &end, vi_word, -1);
+
+    return 1 ;
+}
+
 int main(int argc,const char* argv[]){
+	btinit();
+
+	dic_bt = btopn("AnhViet.dat", 0, 0);
+
+	if(dic_bt == NULL) {
+		printf("Error!\n");
+		exit(0);
+	} 
+
 	gtk_init(&argc, &argv);
 	GtkWidget *window;
 
@@ -63,13 +108,56 @@ int main(int argc,const char* argv[]){
 	GtkWidget *search_entry; 
 	search_entry = gtk_search_entry_new();
 	gtk_widget_set_size_request(search_entry, 450, 0);
-	gtk_layout_put(GTK_LAYOUT(layout), search_entry, 225, 250);
+	gtk_layout_put(GTK_LAYOUT(layout), search_entry, 20, 50);
+
+	GtkWidget *frame;
+	frame = gtk_frame_new (NULL) ;
+	gtk_widget_set_size_request(frame , 590 ,450) ;
+	gtk_layout_put(GTK_LAYOUT(layout) , frame , 20 , 95);
+
+	GtkWidget *text_view;
+	text_view = gtk_text_view_new();
+	gtk_widget_set_size_request(text_view, 580, 440);
+	GtkTextBuffer *buffer;
+	buffer = gtk_text_view_get_buffer(text_view);
+  	gtk_text_buffer_set_text(buffer ,"\nWellcome!", -1);
+  	gtk_text_view_set_justification (text_view , GTK_JUSTIFY_CENTER );
+	gtk_text_view_set_editable (text_view , 0);
+	gtk_layout_put(GTK_LAYOUT(layout), text_view, 25, 100);
+
+	GtkWidget *completion, *autolist;
+	completion = gtk_entry_completion_new() ;
+  	gtk_entry_completion_set_text_column(completion,0);
+  	autolist = gtk_list_store_new(1,G_TYPE_STRING) ;
+  	gtk_entry_completion_set_model(completion,GTK_TREE_MODEL(autolist));
 
 	GtkWidget *button_search;
-	button_search = gtk_button_new_with_label("Search");
-	gtk_widget_set_size_request(button_search, 100, 50);
-	gtk_layout_put(GTK_LAYOUT(layout), button_search, 400, 300);
+	button_search = gtk_button_new_with_label("Search now!");
+	gtk_layout_put(GTK_LAYOUT(layout), button_search, 500, 50);
+	gtk_entry_set_completion(GTK_ENTRY(search_entry),completion);
+	g_signal_connect(search_entry, "activate", G_CALLBACK(Translate_en_vi), text_view);
+	g_signal_connect(G_OBJECT(button_search), "clicked", G_CALLBACK(Translate_en_vi), NULL);
 
+	GtkWidget *add_word;
+	add_word = gtk_button_new_with_label("Add a word!");
+	gtk_widget_set_size_request(add_word, 150, 0);
+	gtk_layout_put(GTK_LAYOUT(layout), add_word, 700, 100);
+	
+	GtkWidget *edit_word;
+	edit_word = gtk_button_new_with_label("Edit this word!");
+	gtk_widget_set_size_request(edit_word, 150, 0);
+	gtk_layout_put(GTK_LAYOUT(layout), edit_word, 700, 150);
+	
+
+	GtkWidget *del_word;
+	del_word = gtk_button_new_with_label("Delete this word!");
+	gtk_widget_set_size_request(del_word, 150, 0);
+	gtk_layout_put(GTK_LAYOUT(layout), del_word, 700, 200);
+	
+	GtkWidget *google_trans;
+	google_trans = gtk_button_new_with_label("Google Translate!");
+	gtk_widget_set_size_request(google_trans, 150, 0);
+	gtk_layout_put(GTK_LAYOUT(layout), google_trans, 700, 525);
 
 	gtk_container_add(GTK_CONTAINER(window) , layout) ;
 	gtk_widget_show_all(window);
