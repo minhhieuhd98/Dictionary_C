0239a4780c0c0b28c95535b940125bb87eb7aa9d
minhhieuhd98 <nguyenminhhieuhd98@gmail.com>
2018-03-29 01:20:53 +0700

    update file main.c

--- main.c
+++ main.c
@@ -9,8 +9,32 @@
 
 BTA* dic_bt;
 
+static char code[128] = { 0 };
 
+const char* soundex(const char *s)
+{
+	static char out[5];
+	int c, prev, i;
 
+	out[0] = out[4] = 0;
+	if (!s || !*s) return out;
+
+	out[0] = *s++;
+
+	/* first letter, though not coded, can still affect next letter: Pfister */
+	prev = code[(int)out[0]];
+	for (i = 1; *s && i < 4; s++) {
+		if ((c = code[(int) * s]) == prev) continue;
+
+		if (c == -1) prev = 0;	/* vowel as separator */
+		else if (c > 0) {
+			out[i++] = c + '0';
+			prev = c;
+		}
+	}
+	while (i < 4) out[i++] = '0';
+	return out;
+}
 char* Search_Word(char *en_word , GtkWidget *search_entry , GtkWidget *text_view){
 	char* data_str = (char*)malloc(sizeof(char)*MAX_LEN);
 	int rsize;
